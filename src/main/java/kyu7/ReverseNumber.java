81f823480c9492aed666462e4dcac1ae20187840
Jarek_k <kolasa.jarek@gmail.com>
2018-08-21 20:43:14 +0200

    kyu 7 - Reverse Number

--- src/main/java/kyu7/ReverseNumber.java
+++ src/main/java/kyu7/ReverseNumber.java
@@ -1,4 +1,34 @@
 package kyu7;
 
 public class ReverseNumber {
+
+
+    public static int reverse(int number) {
+
+        String stringNumbers = Integer.toString(number);
+        StringBuilder newArrayStringBuilder = new StringBuilder();
+
+        char[] array = stringNumbers.toCharArray();
+
+        char[] newArray = new char[array.length];
+
+        if (array[0] != '-') {
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[array.length - i - 1];
+                newArrayStringBuilder.append(newArray[i]);
+            }
+        } else {
+            newArrayStringBuilder.append(array[0]);
+            for (int i = 1; i < array.length; i++) {
+                newArray[i] = array[array.length - i];
+                newArrayStringBuilder.append(newArray[i]);
+            }
+        }
+
+        String newNumberString = newArrayStringBuilder.toString();
+
+        int newNumber = Integer.parseInt(newNumberString);
+
+        return newNumber;
+    }
 }
--- src/test/java/kyu7/ReverseNumberTest.java
+++ src/test/java/kyu7/ReverseNumberTest.java
@@ -1,4 +1,33 @@
 package kyu7;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ReverseNumberTest {
+
+    @Test
+    public void reversePositiveNumberTest(){
+        assertEquals(123, ReverseNumber.reverse(321));
+    }
+
+    @Test
+    public void reverseNegativeNumberTest(){
+        assertEquals(-123, ReverseNumber.reverse(-321));
+    }
+
+    @Test
+    public void reverseFullThousandTest(){
+        assertEquals(1, ReverseNumber.reverse(1000));
+    }
+
+    @Test
+    public void reverseFullMinusThousandTest(){
+        assertEquals(-1, ReverseNumber.reverse(-1000));
+    }
+
+    @Test
+    public void zeroTest(){
+        assertEquals(0, ReverseNumber.reverse(0));
+    }
 }
